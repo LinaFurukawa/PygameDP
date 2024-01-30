6c6e7de816a85f62332186a543d0b1c794c52aad
Guilherme Nakama <guilhermemyn@al.insper.edu>
2024-01-30 13:32:31 -0300

    Ajuste do loop de aumento de objetos diferentes e reboot da caixa de comentário de resposta do jogador

--- game_screen.py
+++ game_screen.py
@@ -1,5 +1,5 @@
 import pygame
-from config import FPS, WIDTH, HEIGHT, BLACK, BLUE, RED
+from config import FPS, WIDTH, HEIGHT, BLACK, BLUE, RED, WHITE
 from assets import carrega_arquivos
 from funcoes import gerar_imagens,  conta_sorteada
 import random
@@ -17,9 +17,9 @@ def game_screen(window):
     tela = 'azul'
     tempo_da_ultima_mudanca = pygame.time.get_ticks()
 
-    lista_imagens = gerar_imagens(2)
+    n = 1
+    lista_imagens = gerar_imagens(n)
     resposta_jogador = ''
-
     pontuacao = 0
 
     # ===== Loop principal =====
@@ -34,33 +34,33 @@ def game_screen(window):
             if event.type == pygame.KEYDOWN:
                 resposta_jogador += event.unicode
                 
-
         # ----- Gera saídas
         window.fill(BLACK)  # Preenche com a cor branca
 
         # Lógica para alternar as cores
         agora = pygame.time.get_ticks()
-        if agora - tempo_da_ultima_mudanca > 2000:
+        if agora - tempo_da_ultima_mudanca > 4000:
             tempo_da_ultima_mudanca = agora
             if tela == 'azul':
                 tela = 'vermelha'
+                
             else:
                 tela = 'azul'
+                resposta_jogador = ''
+                lista_imagens = gerar_imagens(n)
+                n += 1
 
         if tela == 'azul':
             window.fill(BLUE)
 
             sorteada = random.choice(lista_imagens)
-            
-            resposta_jogador = ''
 
             # Desenha as imagens na tela
             for imagem in lista_imagens:
                 window.blit(imagem["imagem"], (imagem["posicao_x"], imagem["posicao_y"]))
 
-
         else:
-            window.fill(RED)
+            window.fill(WHITE)
             # Dimensões do retângulo
             rect_width = WIDTH / 2
             rect_height = HEIGHT / 4
@@ -86,7 +86,6 @@ def game_screen(window):
             window.blit(text_pontuacao, text_pont_rect)
 
             # Adiciona a imagem do tipo que apareceu
-
             window.blit(sorteada["imagem"], (WIDTH / 2 + 100, HEIGHT / 2 - 130))
 
             text_surface = dicionario_de_arquivos['font'].render(resposta_jogador, True, BLUE)
@@ -96,9 +95,7 @@ def game_screen(window):
             window.blit(text_surface, text_rect)
 
             # Verifica se o jogador acertou a quantidade de imagens
-            contagem = str(conta_sorteada(lista_imagens, sorteada))
-            
-            if resposta_jogador == contagem:
+            if resposta_jogador == str(conta_sorteada(lista_imagens, sorteada)):
                 text_surface = dicionario_de_arquivos['font'].render('Acertou!', True, BLUE)
                 text_rect = text_surface.get_rect()
                 text_rect.centerx = WIDTH / 2
@@ -107,22 +104,12 @@ def game_screen(window):
 
                 pontuacao += 100
 
-                
-
-
-
-
             else:
                 text_surface = dicionario_de_arquivos['font'].render('Errou!', True, BLUE)
                 text_rect = text_surface.get_rect()
                 text_rect.centerx = WIDTH / 2
                 text_rect.centery = HEIGHT / 2 + 100
                 window.blit(text_surface, text_rect)
-
-              
-
-
-
 
         pygame.display.update()  # Mostra o novo frame para o jogador
 
